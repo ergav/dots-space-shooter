77f713706071a0f21aa90da2af389c8fed6575e4
ergav <72596720+ergav@users.noreply.github.com>
2024-09-18 16:03:26 +0200

    Started on player death system

--- Assets/Scripts/CollisionSystem.cs
+++ Assets/Scripts/CollisionSystem.cs
@@ -7,7 +7,7 @@ using Unity.Collections;
 using Unity.Transforms;
 using Unity.Mathematics;
 
-public partial struct ProjectileCollissionSystem : ISystem
+public partial struct CollisionSystem : ISystem
 {
     public void OnUpdate(ref SystemState state)
     {
@@ -29,14 +29,31 @@ public partial struct ProjectileCollissionSystem : ISystem
 
                 foreach (ColliderCastHit hit in hits)
                 {
-                    //entityManager.SetEnabled(hit.Entity, false);
+                    entityManager.SetEnabled(hit.Entity, false);
                     //entityManager.SetEnabled(entity, false);
-                    entityManager.DestroyEntity(hit.Entity);
-                    entityManager.DestroyEntity(entity);
+                    //entityManager.DestroyEntity(hit.Entity);
+                    //entityManager.DestroyEntity(entity);
                 }
 
                 hits.Dispose();
-            }          
+            }
+
+            if (entityManager.HasComponent<EnemyTag>(entity))
+            {
+                LocalTransform bulletTransform = entityManager.GetComponentData<LocalTransform>(entity);
+                entityManager.SetComponentData(entity, bulletTransform);
+
+                NativeList<ColliderCastHit> hits = new NativeList<ColliderCastHit>(Allocator.Temp);
+                physicsWorld.SphereCastAll(bulletTransform.Position, 0.5f, float3.zero, 1, ref hits,
+                    new CollisionFilter { BelongsTo = (uint)CollisionLayer.Enemy, CollidesWith = (uint)CollisionLayer.Player });
+
+                foreach (ColliderCastHit hit in hits)
+                {
+                    entityManager.SetEnabled(hit.Entity, false);
+                }
+
+                hits.Dispose();
+            }
         }
     }
 }
@@ -44,5 +61,6 @@ public partial struct ProjectileCollissionSystem : ISystem
 public enum CollisionLayer
 {
     Default = 1 << 0,
-    Enemy = 1 << 6
+    Player = 1 << 3,
+    Enemy = 1 << 6,
 }
--- Assets/Scripts/Player/PlayerMoveSystem.cs
+++ Assets/Scripts/Player/PlayerMoveSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
@@ -29,6 +30,12 @@ public partial struct PlayerMoveJob : IJobEntity
     [BurstCompile]
     private void Execute(ref LocalTransform transform, in PlayerMoveInput input, PlayerMoveSpeed speed)
     {
-        transform.Position.xy += input.value * speed.value * deltaTime;
+        Vector3 pos = transform.Position;
+
+        pos.x += input.value.x * speed.value * deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -8f, 8f);
+
+        transform.Position = pos;
     }
 }
